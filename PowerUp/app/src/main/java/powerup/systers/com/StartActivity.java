123fb817a825bebd472d051b25c2e79bbf5d1933
chhavip <coolaqua12@gmail.com>
2016-01-14 00:04:45 +0530

    Change room on pressing Start

--- PowerUp/app/src/main/java/powerup/systers/com/StartActivity.java
+++ PowerUp/app/src/main/java/powerup/systers/com/StartActivity.java
@@ -8,17 +8,17 @@ import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ImageButton;
 
+
 public class StartActivity extends Activity {
 
+	boolean previouslyStarted;
+	SharedPreferences prefs;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-		boolean previouslyStarted = prefs.getBoolean(getString(R.string.pref_previously_started), false);
-		if (!previouslyStarted) {
-			startActivity(new Intent(StartActivity.this, AvatarRoomActivity.class));
-		}
+		prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+		previouslyStarted = prefs.getBoolean(getString(R.string.pref_previously_started), false);
 		ImageButton new_user = (ImageButton) findViewById(R.id.newUserButtonFirstPage);
 		new_user.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -29,14 +29,25 @@ public class StartActivity extends Activity {
 			}
 		});
 
+
 		ImageButton start = (ImageButton) findViewById(R.id.startButtonMain);
 		start.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent myIntent = new Intent(StartActivity.this, MapActivity.class);
-				startActivityForResult(myIntent, 0);
+				if (!previouslyStarted) {
+					startActivity(new Intent(StartActivity.this, AvatarRoomActivity.class));
+				} else {
+					startActivity(new Intent(StartActivity.this, MapActivity.class));
+				}
+
 			}
 		});
 
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		previouslyStarted = prefs.getBoolean(getString(R.string.pref_previously_started), false);
 	}
 }
