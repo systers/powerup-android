3151080474a8ff49655243b1a4e23782e6c4c12a
chhavip <coolaqua12@gmail.com>
2016-01-19 11:24:50 +0530

    Made required changes

--- PowerUp/app/src/main/java/powerup/systers/com/DisplayPointsActivity.java
+++ PowerUp/app/src/main/java/powerup/systers/com/DisplayPointsActivity.java
@@ -6,37 +6,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import powerup.systers.com.datamodel.SessionHistory;
 
-
-/**
- * Created by chhavi on 9/1/16.
- */
 public class DisplayPointsActivity extends Activity {
-    private TextView currentPoints;
-    private TextView totalPoints;
+    private TextView currentPointsTextView;
+    private TextView totalPointsTextView;
     private Button mapButton;
     private Button nextScenarioButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_points);
-        mapButton = (Button)findViewById(R.id.mapButton);
-        nextScenarioButton = (Button)findViewById(R.id.nextScenarioButton);
-        Intent i = getIntent();
-        int currentPointsValue = i.getExtras().getInt("current_scenario_points");
-        currentPoints = (TextView)findViewById(R.id.current_points_text);
-        totalPoints = (TextView)findViewById(R.id.total_points_text);
-        currentPoints.setText(currentPointsValue + "");
-        totalPoints.setText(SessionHistory.totalPoints + "");
+        mapButton = (Button) findViewById(R.id.mapButton);
+        nextScenarioButton = (Button) findViewById(R.id.nextScenarioButton);
+        Integer currentPointsValue = getIntent().getExtras().getInt(getString(R.string.current_points_key));
+        currentPointsTextView = (TextView) findViewById(R.id.current_points_text);
+        totalPointsTextView = (TextView) findViewById(R.id.total_points_text);
+        currentPointsTextView.setText(currentPointsValue.toString());
+        Integer totalPointsValue = SessionHistory.totalPoints;
+        totalPointsTextView.setText(totalPointsValue.toString());
 
         mapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent returnIntent = new Intent();
-                setResult(Activity.RESULT_CANCELED, returnIntent);
+                setResult(Activity.RESULT_CANCELED, new Intent());
                 finish();
             }
         });
@@ -44,12 +39,10 @@ public class DisplayPointsActivity extends Activity {
         nextScenarioButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent returnIntent = new Intent();
-                setResult(Activity.RESULT_OK, returnIntent);
+                setResult(Activity.RESULT_OK, new Intent());
                 finish();
             }
         });
-
 
 
     }
