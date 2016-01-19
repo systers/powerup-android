14bd65a4d55c8484c720adf5f148e218692b8678
chhavip <coolaqua12@gmail.com>
2016-01-19 11:57:58 +0530

    Made required changes

--- PowerUp/app/src/main/java/powerup/systers/com/StartActivity.java
+++ PowerUp/app/src/main/java/powerup/systers/com/StartActivity.java
@@ -8,46 +8,45 @@ import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ImageButton;
 
-
 public class StartActivity extends Activity {
 
-	boolean previouslyStarted;
-	SharedPreferences prefs;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-		previouslyStarted = prefs.getBoolean(getString(R.string.pref_previously_started), false);
-		ImageButton new_user = (ImageButton) findViewById(R.id.newUserButtonFirstPage);
-		new_user.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent myIntent = new Intent(StartActivity.this,
-						AvatarRoomActivity.class);
-				startActivityForResult(myIntent, 0);
-			}
-		});
+    SharedPreferences preferences;
+    private boolean haspreviouslyStarted;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        haspreviouslyStarted = preferences.getBoolean(getString(R.string.preferences_has_previously_started), false);
+        ImageButton new_user = (ImageButton) findViewById(R.id.newUserButtonFirstPage);
+        new_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(StartActivity.this,
+                        AvatarRoomActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
 
-		ImageButton start = (ImageButton) findViewById(R.id.startButtonMain);
-		start.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (!previouslyStarted) {
-					startActivity(new Intent(StartActivity.this, AvatarRoomActivity.class));
-				} else {
-					startActivity(new Intent(StartActivity.this, MapActivity.class));
-				}
+        ImageButton start = (ImageButton) findViewById(R.id.startButtonMain);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (haspreviouslyStarted) {
+                    startActivity(new Intent(StartActivity.this, MapActivity.class));
+                } else {
+                    startActivity(new Intent(StartActivity.this, AvatarRoomActivity.class));
+                }
 
-			}
-		});
+            }
+        });
 
-	}
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		previouslyStarted = prefs.getBoolean(getString(R.string.pref_previously_started), false);
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        haspreviouslyStarted = preferences.getBoolean(getString(R.string.preferences_has_previously_started), false);
+    }
 }
