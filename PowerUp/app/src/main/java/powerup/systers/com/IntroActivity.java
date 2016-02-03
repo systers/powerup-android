dec6cd579c8d6f4790a526130e4be6bee4c2e1bb
medhach <kmit.medhach@gmail.com>
2016-02-03 09:24:43 +0530

    Introduction Screens

---
+++ PowerUp/app/src/main/java/powerup/systers/com/IntroActivity.java
@@ -0,0 +1,63 @@
+package powerup.systers.com;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+public class IntroActivity extends AppIntro {
+
+    private SharedPreferences sharedPreferences;
+    private boolean isfirstRun;
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+        sharedPreferences = getSharedPreferences("prefs", 0);
+        isfirstRun = sharedPreferences.getBoolean("firstRun", true);
+        if (isfirstRun) {
+            addSlide(new IntroFragment(1));
+            addSlide(new IntroFragment(2));
+            addSlide(new IntroFragment(3));
+            addSlide(new IntroFragment(4));
+            addSlide(new IntroFragment(5));
+            addSlide(new IntroFragment(6));
+        } else {
+            loadMainActivity();
+        }
+    }
+
+    private void loadMainActivity() {
+        sharedPreferences = getSharedPreferences("prefs", 0);
+        SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+        sharedPreferencesEditor.putBoolean("firstRun", false);
+        sharedPreferencesEditor.commit();
+        startActivity(new Intent(this, StartActivity.class));
+        finish();
+    }
+
+    @Override
+    public void onSkipPressed() {
+        loadMainActivity();
+    }
+
+    @Override
+    public void onNextPressed() {
+
+    }
+
+    @Override
+    public void onDonePressed() {
+        loadMainActivity();
+    }
+
+    @Override
+    public void onSlideChanged() {
+
+    }
+
+
+}
