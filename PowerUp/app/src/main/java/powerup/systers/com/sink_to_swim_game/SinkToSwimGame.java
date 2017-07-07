34663e0fec990573f4652cd2125240b6d560fd7f
codingblazer <sasachin04@gmail.com>
2017-07-07 20:37:18 +0530

    Swim Sink Game working, Animations Left

--- PowerUp/app/src/main/java/powerup/systers/com/sink_to_swim_game/SinkToSwimGame.java
+++ PowerUp/app/src/main/java/powerup/systers/com/sink_to_swim_game/SinkToSwimGame.java
@@ -1,6 +1,7 @@
 package powerup.systers.com.sink_to_swim_game;
 
-import android.animation.Animator;
+
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,173 +11,173 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import powerup.systers.com.GameActivity;
 import powerup.systers.com.R;
+import powerup.systers.com.datamodel.Score;
 import powerup.systers.com.powerup.PowerUpUtils;
 
 public class SinkToSwimGame extends AppCompatActivity {
     //make session manager for this
     //handle case of interruption..timer lifecycle
-    //addd animations at last
+    //addd animations at last - score, boat, fade in, checkmark, timer,
     //do formatting
     //add tests + other Power Up work like setting up scenarios
+    //make the sceanrio content flowcharts
+    //include this mini game into the main power up game
 
     FrameLayout.LayoutParams lp;
     ImageView pointer, boat;
     int height;
     private Animation mAnimation;
-    int score, curQuestion, timerValue;
-    SinkSwimGameTimeCounter counter;
+    int score, curQuestion;
     Button trueOption, falseOption, skipOption;
-    TextView questionView;
+    TextView questionView, timer, scoreView;
+    long questionStartTime;
+    long millisleft;
+    CountDownTimer countDownTimer;
+    int speed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sink_to_swim_game);
 
-        mAnimation = AnimationUtils.loadAnimation(this, R.animator.boat_animation);
+        mAnimation = AnimationUtils.loadAnimation(this, R.animator.boat_animation); // Now it should work smoothy as hanging problem is solved now
         mAnimation.setInterpolator(new LinearInterpolator());
         boat = (ImageView) findViewById(R.id.boat);
+        boat.startAnimation(mAnimation);
         trueOption = (Button) findViewById(R.id.true_option);
         skipOption = (Button) findViewById(R.id.skip_option);
         falseOption = (Button) findViewById(R.id.false_option);
-        counter = new SinkSwimGameTimeCounter(300,300);
+
         questionView = (TextView) findViewById(R.id.questionView);
+        scoreView = (TextView) findViewById(R.id.swim_score);
+        timer = (TextView) findViewById(R.id.time);
         initialSetUp();
     }
 
     public void initialSetUp() {
         bringPointerToCenter();
         score = 0;
+        speed = 2;
         curQuestion = -1;
-        timerValue = 30; //sec
+        millisleft = 60000; //=30 sec
+        questionStartTime = millisleft;
         trueOption.setClickable(false);
         falseOption.setClickable(false);
         skipOption.setClickable(false);
+        boat.setTranslationY(-height*0.05f);
         showNextQuestion();
-//        counter.start();
+        countDownTimer = new CountDownTimer(millisleft, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                millisleft = millisUntilFinished;
+                timer.setText("" + millisleft / 1000);
+                bringPointerAndAvatarDown();
+                if (getMargin() > height * 0.75){ // edit this with actual value
+                    gameEnd();
+                }
+            }
+
+            public void onFinish() {
+                gameEnd();
+            }
+        };
+        countDownTimer.start();
+
+
     }
 
-    public void answerChosen(View view){
+    private void gameEnd() {
+        countDownTimer.cancel();
+        startActivity(new Intent(SinkToSwimGame.this, GameActivity.class));
+    }
+
+    private void showNextQuestion() {
+        questionStartTime = millisleft;
+        curQuestion++;
+        if (curQuestion == PowerUpUtils.SWIM_SINK_QUESTION_ANSWERS.length)
+            curQuestion = 0;
+        new Runnable() {
+            @Override
+            public void run() {
+                questionView.setText(PowerUpUtils.SWIM_SINK_QUESTION_ANSWERS[curQuestion][0]);
+            }
+        }.run();
+
+        trueOption.setClickable(true);
+        falseOption.setClickable(true);
+        skipOption.setClickable(true);
+    }
+
+    public void answerChosen(View view) {
         trueOption.setClickable(false);
         falseOption.setClickable(false);
         skipOption.setClickable(false);
-        if (view == findViewById(R.id.true_option)){
-            if(PowerUpUtils.SWIM_SINK_QUESTION_ANSWERS[curQuestion][1]=="T"){
-                
+        //replace this by switch case and ?: ternary operator
+        if (view == findViewById(R.id.true_option)) {
+            if (PowerUpUtils.SWIM_SINK_QUESTION_ANSWERS[curQuestion][1] == "T") {
+                score += 1 + questionStartTime / 1000 - millisleft / 1000;
+                increaseonePointerAvatarUpOneLevel();
+            } else {
+                score += questionStartTime / 1000 - millisleft / 1000 - 1;
             }
-
-        }else if (view == findViewById(R.id.false_option)){
-            if(PowerUpUtils.SWIM_SINK_QUESTION_ANSWERS[curQuestion][1]=="F"){
-
+        } else if (view == findViewById(R.id.false_option)) {
+            if (PowerUpUtils.SWIM_SINK_QUESTION_ANSWERS[curQuestion][1] == "F") {
+                score += 1 + questionStartTime / 1000 - millisleft / 1000;
+                increaseonePointerAvatarUpOneLevel();
+            } else {
+                score += questionStartTime / 1000 - millisleft / 1000 - 1;
             }
-        }else {
-
+        } else {
         }
-
-        //first disable all three buttons - true false ?
-        //check if true or false or no guess
-        //fetch answer of question from, if correct +1, otherwise -1 and reflect it on
-        //if correct => +1 and shot him up by one level, wrong, shoot him down by 1 point
-        //
+        showNextQuestion();
+        scoreView.setText("Score: " + score);
     }
 
     private void bringPointerToCenter() {
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        height =  displayMetrics.heightPixels;
+        height = displayMetrics.heightPixels;
         pointer = (ImageView) findViewById(R.id.pointer);
 
         lp = new FrameLayout.LayoutParams(pointer.getLayoutParams());
-        double initial_height =  height* 0.50;
-        lp.setMargins(0,(int) initial_height, 0, 0);
+//        double initial_height = height * 0.50;
+        double initial_height = height * 0.10;
+        lp.setMargins(0, (int) initial_height, 0, 0);
         pointer.setLayoutParams(lp);
     }
 
-    public void decreaseonePointerDownOneLevel(){
-        lp = new FrameLayout.LayoutParams(pointer.getLayoutParams());
-        double pixels = height * 0.1;
-        int total = (int) pixels + getMargin();
-        lp.setMargins(0,total,0,0);
-        pointer.setLayoutParams(lp);
-    }
-    public void increaseonePointerUpOneLevel(){
+    public void increaseonePointerAvatarUpOneLevel() {
+        if (getMargin() < height * 0.19)
+            return;
         lp = new FrameLayout.LayoutParams(pointer.getLayoutParams());
         double pixels = height * 0.1;
         int total = getMargin() - (int) pixels;
-        lp.setMargins(0,total,0,0);
+        lp.setMargins(0, total, 0, 0);
         pointer.setLayoutParams(lp);
-    }
+        boat.animate().translationYBy(-(height*0.1f*0.66f));
 
-    private void showNextQuestion() {
-        curQuestion ++;
-        questionView.setText(PowerUpUtils.SWIM_SINK_QUESTION_ANSWERS[curQuestion][0]);
-        trueOption.setClickable(true);
-        falseOption.setClickable(true);
-        skipOption.setClickable(true);
-//        questionView.animate().alpha(0).setDuration(30).setListener(new Animator.AnimatorListener() {
-//            @Override
-//            public void onAnimationStart(Animator animator) {
-//
-//            }
-//
-//            @Override
-//            public void onAnimationEnd(Animator animator) {
-//
-//                questionView.animate().alpha(1).setDuration(30).start();
-//
-//            }
-//
-//            @Override
-//            public void onAnimationCancel(Animator animator) {
-//
-//            }
-//
-//            @Override
-//            public void onAnimationRepeat(Animator animator) {
-//
-//            }
-//        });
+    }
+    public void bringPointerAndAvatarDown() {
+        lp = new FrameLayout.LayoutParams(pointer.getLayoutParams());
+        double pixels = height * 0.01 * speed;
+        int total = getMargin() + (int) pixels;
+        lp.setMargins(0, total, 0, 0);
+        pointer.setLayoutParams(lp);
+        boat.animate().translationYBy(height*0.01f*speed*0.66f);
     }
 
     public int getMargin() {
-        FrameLayout.LayoutParams newparams =  (FrameLayout.LayoutParams) pointer.getLayoutParams();
+        FrameLayout.LayoutParams newparams = (FrameLayout.LayoutParams) pointer.getLayoutParams();
         return newparams.topMargin;
     }
 
-    @Override
-    protected void onResume() {
-
-        boat.startAnimation(mAnimation);
-
-        super.onResume();
-    }
-
-
-    public class SinkSwimGameTimeCounter extends CountDownTimer {
-
-        public SinkSwimGameTimeCounter(long millisInFuture, long countDownInterval) {
-            super(millisInFuture, countDownInterval);
-        }
-
-        @Override
-        public void onTick(long l) {
-            //change score
-            //change timer text
-            //change
-        }
-
-        @Override
-        public void onFinish() {
-
-        }
-
-
-    }
 
 }
