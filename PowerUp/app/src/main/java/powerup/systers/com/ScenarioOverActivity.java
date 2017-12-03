96144c50f2e2db2825f7c5c1c961f84c9d0dde3a
Aryaman21 <aryaman.agrawal21@gmail.com>
2017-12-03 18:18:07 +0530

    BackPress GCI (with fixed spaces)

--- PowerUp/app/src/main/java/powerup/systers/com/ScenarioOverActivity.java
+++ PowerUp/app/src/main/java/powerup/systers/com/ScenarioOverActivity.java
@@ -96,6 +96,7 @@ public class ScenarioOverActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         new GameActivity().gameActivityInstance.finish();
+        startActivity(new Intent(this, MapActivity.class));
     }
 
     public DatabaseHandler getmDbHandler() {
--- PowerUp/app/src/main/java/powerup/systers/com/sink_to_swim_game/SinkToSwimEndActivity.java
+++ PowerUp/app/src/main/java/powerup/systers/com/sink_to_swim_game/SinkToSwimEndActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import powerup.systers.com.GameOverActivity;
 import powerup.systers.com.R;
+import powerup.systers.com.StartActivity;
 import powerup.systers.com.powerup.PowerUpUtils;
 
 public class SinkToSwimEndActivity extends AppCompatActivity {
@@ -32,5 +33,9 @@ public class SinkToSwimEndActivity extends AppCompatActivity {
         Intent intent = new Intent(SinkToSwimEndActivity.this, GameOverActivity.class);
         finish();
         startActivityForResult(intent, 0);
+    }
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(this, StartActivity.class));
     }
 }
