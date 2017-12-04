4338b4f9d52b59f886fc3c21fa28f5b48eca2a71
Abhijeet Manohar <abhijeetheromanohar@gmail.com>
2017-12-04 20:54:20 +0530

    Revert "GCI: Program:GCI Include total karma points on final screen of PowerUp Android"
    This reverts commit 5cfe0d7d05d09a8b8492aec6896654d3ef09d3c2.

--- PowerUp/app/src/main/java/powerup/systers/com/GameOverActivity.java
+++ PowerUp/app/src/main/java/powerup/systers/com/GameOverActivity.java
@@ -10,9 +10,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import powerup.systers.com.datamodel.SessionHistory;
 
 public class GameOverActivity extends Activity {
 
@@ -23,8 +20,6 @@ public class GameOverActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.completed_game);
-        TextView karmaPoints = (TextView) findViewById(R.id.karmaPoints);
-        karmaPoints.setText(String.valueOf(SessionHistory.totalPoints));
         Button backToMap = (Button) findViewById(R.id.ContinueButtonMap);
         backToMap.setOnClickListener(new View.OnClickListener() {
             @Override
