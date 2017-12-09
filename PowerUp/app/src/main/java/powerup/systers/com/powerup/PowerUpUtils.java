5bd69d85a345bfd7288ad3acc1fd9d41c952ff61
Isabel Sieh <isabelsieh@gmail.com>
2017-12-09 23:16:25 +0800

    Ordering Items based on Karma Points

--- PowerUp/app/src/main/java/powerup/systers/com/powerup/PowerUpUtils.java
+++ PowerUp/app/src/main/java/powerup/systers/com/powerup/PowerUpUtils.java
@@ -46,13 +46,14 @@ public class PowerUpUtils {
     public static final int[] VOCAB_MATCH_TUTS = {R.drawable.vocab_tut1,R.drawable.vocab_tut2};
     public static final int[] MINES_TUTS = {R.drawable.mines_tut1,R.drawable.mines_tut2,R.drawable.mines_tut3};
 
-    public static final int[] HAIR_IMAGES = {R.drawable.hair1,R.drawable.hair2,R.drawable.hair3,R.drawable.hair4,R.drawable.hair5,R.drawable.hair6,R.drawable.hair7,R.drawable.hair8,R.drawable.hair9,R.drawable.hair10,R.drawable.hair11,R.drawable.hair12,R.drawable.hair13,R.drawable.hair14,R.drawable.hair15,R.drawable.hair16};
-    public static final String[] HAIR_POINTS_TEXTS = {"5","5","5","10","10","10","5","5","10","5","5","10","10","10","5","5"};
+
+    public static final int[] HAIR_IMAGES = {R.drawable.hair1,R.drawable.hair2,R.drawable.hair3,R.drawable.hair7,R.drawable.hair8,R.drawable.hair10,R.drawable.hair11,R.drawable.hair15,R.drawable.hair16,R.drawable.hair4,R.drawable.hair5,R.drawable.hair6,R.drawable.hair9,R.drawable.hair12,R.drawable.hair13,R.drawable.hair14};
+    public static final String[] HAIR_POINTS_TEXTS = {"5","5","5","5","5","5","5","5","5","10","10","10","10","10","10","10"};
 
     public static final int[] CLOTHES_IMAGES = {R.drawable.dress1,R.drawable.dress2,R.drawable.dress3,R.drawable.dress4};
     public static final String[] CLOTHES_POINTS_TEXTS = {"5","10","10","10"};
 
-    public static final int[] ACCESSORIES_IMAGES = {R.drawable.acc1,R.drawable.acc2,R.drawable.acc3,R.drawable.acc4};
-    public static final String[] ACCESSORIES_POINTS_TEXTS = {"10","5","5","10"};
+    public static final int[] ACCESSORIES_IMAGES = {R.drawable.acc2,R.drawable.acc3,R.drawable.acc1,R.drawable.acc4};
+    public static final String[] ACCESSORIES_POINTS_TEXTS = {"5","5","10","10"};
 }
 
