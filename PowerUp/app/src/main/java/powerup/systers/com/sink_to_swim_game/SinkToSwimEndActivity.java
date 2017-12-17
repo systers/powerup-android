04d3666bc96a910f9f991260517e18a1124b5397
Abhijeet Manohar <abhijeetheromanohar@gmail.com>
2017-12-17 21:03:03 +0800

    Add mini games score to karma points
    Added score of 3 mini games and displayed it .

--- PowerUp/app/src/main/java/powerup/systers/com/sink_to_swim_game/SinkToSwimEndActivity.java
+++ PowerUp/app/src/main/java/powerup/systers/com/sink_to_swim_game/SinkToSwimEndActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import powerup.systers.com.GameOverActivity;
 import powerup.systers.com.R;
 import powerup.systers.com.powerup.PowerUpUtils;
+import powerup.systers.com.datamodel.SessionHistory;
 
 public class SinkToSwimEndActivity extends AppCompatActivity {
 
@@ -26,6 +27,8 @@ public class SinkToSwimEndActivity extends AppCompatActivity {
         scoreView.setText(""+score);
         correctView.setText(""+correctAnswers);
         wrongView.setText(""+wrongAnswers);
+        SessionHistory.totalPoints+=score;
+        SessionHistory.currScenePoints+=score;
     }
 
     public void continuePressed(View view){
