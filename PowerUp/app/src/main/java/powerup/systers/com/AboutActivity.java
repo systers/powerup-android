8899e415431d5a8d5314032c19db62f201f64f3f
Aryaman21 <aryaman.agrawal21@gmail.com>
2017-12-17 05:37:31 +0530

    Program GCI: Inclding Log Messages
    Commit as part of the GCI task 'Create Log messages for Android application'

--- PowerUp/app/src/main/java/powerup/systers/com/AboutActivity.java
+++ PowerUp/app/src/main/java/powerup/systers/com/AboutActivity.java
@@ -2,11 +2,13 @@ package powerup.systers.com;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 
+    private static final String TAG = "Mymessage";
     private boolean isAboutGameOpen = false;
     private boolean isAboutUrgencyOpen = false;
     private boolean isAboutHelpingOpen = false;
@@ -16,6 +18,9 @@ public class AboutActivity extends Activity {
     private TextView aboutGameSection, aboutUrgencySection, aboutHelpingSection;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG,"Building the AboutActivity");
+        Log.v(TAG,"Verbose for Building Activity");
+        Log.i(TAG,"About Activity Awakening");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         if (savedInstanceState != null){
@@ -27,6 +32,7 @@ public class AboutActivity extends Activity {
         aboutUrgencySection = (TextView) findViewById(R.id.about_the_urgency);
         aboutHelpingSection = (TextView) findViewById(R.id.about_helping_by);
         if (isAboutGameOpen){
+            Log.d(TAG,"Checking Visibility of Game Section");
            aboutGameSection.setVisibility(View.VISIBLE);
         }
         if (isAboutUrgencyOpen){
@@ -38,6 +44,9 @@ public class AboutActivity extends Activity {
 
     }
     public void aboutGamePressed(View view){
+        Log.d(TAG,"Info about the game");
+        Log.i(TAG,"Game Info will Open or Close");
+        Log.w(TAG,"Game Info will Open or Close");
         if (aboutGameSection.getVisibility() == View.GONE){
             aboutGameSection.setVisibility(View.VISIBLE);
             isAboutGameOpen = true;
@@ -48,6 +57,8 @@ public class AboutActivity extends Activity {
     }
 
     public void aboutUrgencyPressed(View view){
+        Log.d(TAG,"Info about the urgency of the game");
+        Log.v(TAG,"Info about the urgency true or false = open or close");
         if (aboutUrgencySection.getVisibility() == View.GONE){
             aboutUrgencySection.setVisibility(View.VISIBLE);
             isAboutUrgencyOpen = true;
@@ -58,6 +69,8 @@ public class AboutActivity extends Activity {
     }
 
     public void aboutHelpingByPressed(View view){
+        Log.d(TAG,"Info about how the game helps teenager");
+        Log.i(TAG,"Helping Info will open or close");
         if (aboutHelpingSection.getVisibility() == View.GONE){
             aboutHelpingSection.setVisibility(View.VISIBLE);
             isAboutHelpingOpen = true;
@@ -68,11 +81,15 @@ public class AboutActivity extends Activity {
     }
 
     public void pressHomeButton(View view){
+        Log.d(TAG,"Destroying Activity, Going to StartActivity");
+        Log.w(TAG,"AboutActvity to be destroyed");
         finish();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG,"Saving the status of info");
+        Log.i(TAG,"Saving the status of info");
         super.onSaveInstanceState(outState);
         outState.putBoolean(isGameOpen,isAboutGameOpen);
         outState.putBoolean(isHelpingOpen,isAboutHelpingOpen);
