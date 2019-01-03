19e267715ce30e3f373ce80fabc2a2a8ae88c3fb
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2019-01-03 23:48:49 +0530

    restoring points after removing the app from background

--- PowerUp/app/src/main/java/powerup/systers/com/data/dao/PointsDao.java
+++ PowerUp/app/src/main/java/powerup/systers/com/data/dao/PointsDao.java
@@ -2,12 +2,17 @@ package powerup.systers.com.data.dao;
 
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
 
 import powerup.systers.com.data.entities.Points;
 
 @Dao
 public interface PointsDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertPoints(Points points);
+
+    @Query("SELECT * FROM Point")
+    Points getPoints();
 }
